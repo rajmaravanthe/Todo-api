ee5c5c3506f69cfca83b334dd60df9d79cfa1894
rajesha maravanthe <rraajjpoojary@gmail.com>
2017-01-02 17:11:27 +0530

    Convert DELETE /todos/id to sqlite

--- server.js
+++ server.js
@@ -60,13 +60,22 @@ app.post('/todos', function (req, res) {
 
 app.delete('/todos/:id', function (req, res) {
     var todoId = parseInt(req.params.id);
-    var matchedTodo = _.findWhere(todos, { id: todoId });
-    if (!matchedTodo) {
-        res.status(404).json({ "error": "no todo found in ID" });
-    } else {
-        todos = _.without(todos, matchedTodo);
-        res.json(matchedTodo);
-    }
+    var todoId = parseInt(req.params.id);
+    db.todo.destroy({
+        where: {
+            id: todoId
+        }
+    }).then(function (rowsDeleted) {
+        if(rowsDeleted === 0) {
+            res.status(404).json({
+                error: 'No todo with id'
+            });
+        } else {
+            res.status(204).send();
+        }
+    }, function() {
+        res.status(500).send();
+    });
 });
 
 app.put('/todos/:id', function (req, res) {
